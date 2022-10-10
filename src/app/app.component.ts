46aa8ea57567b5bc0b4ecbfadd49a71839c25f2a
“sadikturan” <sadikturan@gmail.com>
2022-10-10 11:52:53 +0300

    ders 5: component selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app',
+  // selector: '.app',
+  selector: '#app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
