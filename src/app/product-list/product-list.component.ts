8e32c621739204187e48e19d3cea361d3e5c7c04
“sadikturan” <sadikturan@gmail.com>
2022-10-13 12:49:47 +0300

    Ders 12: Ng Templates

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,43 +12,15 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  product = {
-    id: 1,
-    name: "iphone 14",
-    price: 20000,
-    imageUrl: "1.jpeg",
-    description: "iyi telefon",
-    isActive: true
-  }
+  private products : any[] = [
 
-  productList= ["iphone 14", "iphone 15", "iphone 16","iphone 17"];
-
-  products:any= [
-    {
-      id: 1,
-      name: "iphone 14",
-      price: 20000,
-      imageUrl: "1.jpeg",
-      description: "iyi telefon",
-      isActive: false
-    },
-    {
-      id: 2, 
-      name: "iphone 15",
-      price: 30000,
-      imageUrl: "2.jpeg",
-      description: "iyi telefon",
-      isActive: false
-    }
-    ,
-    {
-      id: 2,
-      name: "iphone 16",
-      price: 30000,
-      imageUrl: "3.jpeg",
-      description: "iyi telefon",
-      isActive: true
-    }
+    { id: 1, name: "iphone 14", price: 20000, imageUrl: "1.jpeg", description: "iyi telefon", isActive: true },
+    { id: 2, name: "iphone 15", price: 30000, imageUrl: "2.jpeg", description: "iyi telefon", isActive: true },
+    { id: 3, name: "iphone 16", price: 40000, imageUrl: "3.jpeg", description: "iyi telefon", isActive: true }   
   ];
+
+  getProducts() {
+    return this.products.filter(p=>p.isActive);
+  }
 
 }
