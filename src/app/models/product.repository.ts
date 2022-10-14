c1838536cdf2865e0994e20a64b4a80b99289e27
“sadikturan” <sadikturan@gmail.com>
2022-10-14 11:27:52 +0300

    Ders 13: models

---
+++ src/app/models/product.repository.ts
@@ -0,0 +1,17 @@
+import { Product } from "./product";
+
+export class ProductRepository {
+    private products : Product[] = [
+        { id: 1, name: "iphone 14", price: 20000, imageUrl: "1.jpeg", description: "iyi telefon", isActive: true },
+        { id: 2, name: "iphone 15", price: 30000, imageUrl: "2.jpeg", description: "iyi telefon", isActive: true },
+        { id: 3, name: "iphone 16", price: 40000, imageUrl: "3.jpeg", description: "iyi telefon", isActive: true }   
+    ];
+
+    getProducts(): Product[] {
+        return this.products.filter(p=>p.isActive);
+    }
+
+    getProductById(id: number) {
+        return this.products.find(p=>p.id == id);
+    }
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { ProductRepository } from '../models/product.repository';
 
 @Component({
   selector: 'product-list',
@@ -7,20 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+  productRepository: ProductRepository;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.productRepository = new ProductRepository();
+    this.products = this.productRepository.getProducts();
   }
 
-  private products : any[] = [
-
-    { id: 1, name: "iphone 14", price: 20000, imageUrl: "1.jpeg", description: "iyi telefon", isActive: true },
-    { id: 2, name: "iphone 15", price: 30000, imageUrl: "2.jpeg", description: "iyi telefon", isActive: true },
-    { id: 3, name: "iphone 16", price: 40000, imageUrl: "3.jpeg", description: "iyi telefon", isActive: true }   
-  ];
-
-  getProducts() {
-    return this.products.filter(p=>p.isActive);
+  ngOnInit(): void {
   }
 
 }
